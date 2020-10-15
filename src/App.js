ef569823ec5028cfe291686ecd19d5eb80cc1d72
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-15 11:30:33 +0200

    Added Todo Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import ToDoForm from "./components/ToDoForm";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <ToDoForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ToDoForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function ToDoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //props.onSubmit({
+    // id: Math.floor(Math.random() * 10000),
+    // text: input,
+    // });
+    setInput("");
+  };
+
+  return (
+    <div className="todoform">
+      <form onSubmit={handleSubmit} className="todoform__form">
+        <input
+          type="text"
+          placeholder="Type..."
+          value={input}
+          name="text"
+          className="todoform__input"
+          onChange={handleChange}
+        />
+        <button className="todoform__btn">ADD</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoForm;
